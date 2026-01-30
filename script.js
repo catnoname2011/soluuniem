19f3bb2c19416ba5475b27844abeaaf41b3e9d3a
catnoname2011 <p0941265935@gmail.com>
2026-01-30 22:21:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,29 @@
-const openPost = document.getElementById('openPost');
-const postBox = document.getElementById('postBox');
-const pasteArea = document.getElementById('pasteArea');
-const preview = document.getElementById('preview');
-let imageData = "";
+const api = 'https://api.github.com/repos/catnoname2011/soluuniem/issues';
 
 
-openPost.onclick = () => postBox.classList.toggle('hidden');
-
-
-pasteArea.addEventListener('paste', e => {
-const item = e.clipboardData.items[0];
-if (item.type.indexOf('image') !== -1) {
-const file = item.getAsFile();
-const reader = new FileReader();
-reader.onload = () => {
-imageData = reader.result;
-preview.src = imageData;
-preview.style.display = 'block';
-};
-reader.readAsDataURL(file);
-}
-});
-
-
-function submitPost() {
-const title = document.getElementById('title').value;
-const content = document.getElementById('content').value;
-const posts = JSON.parse(localStorage.getItem('posts') || '[]');
-posts.unshift({ title, content, image: imageData });
-localStorage.setItem('posts', JSON.stringify(posts));
-location.reload();
-}
-
-
-function loadPosts() {
-const posts = JSON.parse(localStorage.getItem('posts') || '[]');
+fetch(api)
+.then(res => res.json())
+.then(data => {
 const box = document.getElementById('posts');
-posts.forEach(p => {
+box.innerHTML = '';
+
+
+data.forEach(issue => {
 const div = document.createElement('div');
 div.className = 'post';
-div.innerHTML = `<h3>${p.title}</h3><p>${p.content}</p>`;
-if (p.image) div.innerHTML += `<img src="${p.image}">`;
+
+
+let body = issue.body || '';
+body = body.replace(/\n/g, '<br>');
+
+
+div.innerHTML = `
+<h3>${issue.title}</h3>
+<p>${body}</p>
+<a target="_blank" href="${issue.html_url}">💬 Bình luận</a>
+`;
+
+
 box.appendChild(div);
 });
-}
-
-
-loadPosts();
+});
